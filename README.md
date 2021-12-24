302e79546c16def60edc6a7dedbd2686c07fbeef
Paul Dawoon Kim <91786930+ITDawoon@users.noreply.github.com>
2021-12-25 01:27:17 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,88 @@
+# InsulinGlucagonPump
+
+
+## CP3407- Project Description
+
+Insulin pump is a medical system that simulates the movement of the pancreas. The software that controls this system is a built-in system that collects information from sensors and controls the pump that transmits a controlled amount of insulin to users. 
+People with diabetes use this system. Diabetes is a common condition in which the pancreas cannot produce a hormone called insulin. Insulin metabolizes glucose in the blood. The conventional treatment for diabetes is regular injections of genetically modified insulin. Diabetics use an external meter to measure blood sugar and calculate the amount of insulin they should inject.
+The problem with this treatment is that the insulin level required depends not only on the blood glucose level, but also on the last insulin injection time. This can significantly lower blood glucose levels or increase blood glucose levels (when insulin is too high). Hypoglycemia is more serious in the short run because it can lead to temporary brain malfunctions and eventually unconsciousness and death. However, if blood glucose levels continue to rise in the long run, eye damage, kidney damage, and heart disease may occur.
+
+## UML Diagram
+
+![InsulinGlucagonPump_UML](https://user-images.githubusercontent.com/58071533/130734918-f90673d7-c2e3-4355-a114-3759d34b75f9.png)
+
+
+## Technology Used
+1. Java Programming
+2. JFreeChart Java
+3. Java AWT as UI
+4. Java DB MYSQL Connector
+5. MySQL Server as backend
+6. MD5 hash for password hashing and storing
+
+## Running the Project Locally guide
+1. Install MySQL: https://dev.mysql.com/downloads/mysql/
+    - Create a user "mysql" with password "mysql", This is vital because the main connector config for MYSQL within the App using this credentials. You can change this within the codes if you insist
+    - Set up PATH variable in your operating system if the shell couldn't find "mysql" command
+
+
+2. To inspect the database, I would recommend you using this software call Tableplus: https://tableplus.com/. From here you can inspect the data of the user that take the Insulin Simulator test.
+
+
+3. Clone this Repo at:
+  ```sh
+    git clone https://github.com/minhquan0902/CP3407-Assignment-1.git
+  ```
+4. Open the Project with any IDE (VScode, IntelliJ or Eclipse,...)
+
+
+5. Add the dependencies and libraries if the IDE cannot detect them automatically.
+
+6. Open the Terminal and create the database called InsulinPump
+  ```sh
+    $mysql -u mysql -p mysql
+  ```
+  
+  ```sh
+    mysql> CREATE DATABASE InsulinPump;
+  ```
+  
+7. Connect to the Database InsulinPump 
+
+  ```sh
+    mysql> connect InsulinPump
+  ```
+  
+8. Create new table called users for storing users
+  
+  ```sh
+    mysql> CREATE TABLE users
+            (
+            id int NOT NULL AUTO_INCREMENT,
+            username varchar(255) NOT NULL UNIQUE,
+            email varchar(255) NOT NULL UNIQUE,
+            password varchar(255),
+            PRIMARY KEY (id)
+            );
+  ```
+9. Create another table called users_detail for user detail
+
+ ```sh
+    mysql> CREATE TABLE users_detail
+            (
+            id int NOT NULL AUTO_INCREMENT,
+            username varchar(255) NOT NULL UNIQUE,
+            email varchar(255) NOT NULL UNIQUE,
+            age   varchar(255),
+            weight varchar(255),
+            height varchar(255),
+            PRIMARY KEY (id)
+            );
+  ```
+  
+10. To view Table content, connect to the Database InsulinPump and simply query
+
+  ```sh
+    mysql> select * from Persons
+  ```
+  
---
+++ src/bin/InsulinGlucagonPump.java
@@ -0,0 +1,165 @@
+package bin;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.sql.*;
+import java.util.Objects;
+import javax.swing.*;
+
+
+public class InsulinGlucagonPump extends JFrame {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected static final Component PanelManual = null;
+
+    public InsulinGlucagonPump() {
+
+        JButton Automatic;
+        JButton RegisterButton;
+       
+        
+        setTitle("Insulin/Glucagon Pump");
+        setSize(400, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+
+        setLayout(new BorderLayout());
+
+        setLayout(new FlowLayout());
+
+
+
+        Automatic = new JButton("Start the Simulator");
+        RegisterButton = new JButton ("Register for an account if you hadn't had one :)");
+        // Info Panel
+        JLabel info = new JLabel("Taking in your credentials help us save your data for future tracing");
+        info.setBounds(10,50, 50, 180);
+        add(info);
+        // Login panel
+        JLabel userLabel = new JLabel("Input Username ");
+        userLabel.setBounds(10,20,80,25);
+        add(userLabel);
+        JTextField usernameText = new JTextField(20);
+        usernameText.setBounds(100,20,165,25);
+        add(usernameText);
+        setVisible(true);
+
+        // Login panel
+        JLabel passwordLabel = new JLabel("Input Password ");
+        passwordLabel.setBounds(10,20,100,25);
+        add(passwordLabel);
+        JPasswordField passwordText = new JPasswordField(20);
+        passwordText.setBounds(100,20,165,25);
+        add(passwordText);
+        setVisible(true);
+
+        add(Automatic);
+        setSize(399, 399);
+        setSize(400, 400);
+
+        add(RegisterButton);
+        setSize(399, 399);
+        setSize(400, 400);
+
+
+        RegisterButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new RegisterForm();
+            }
+        });
+
+        Automatic.addActionListener(new ActionListener() {
+            
+                @Override
+            public void actionPerformed(ActionEvent e) {
+               
+
+                String username = usernameText.getText();
+                String password = passwordText.getText();
+
+
+
+                // Login
+                Boolean loginState = login(username, password, false);
+
+                if (loginState){
+                    new BloodSugar();
+                    new Monitor();
+                    new GUI("Auto");
+                    JFrame g = new JFrame();
+                    JOptionPane.showMessageDialog(g,"Login successfully :) ");
+                }else {
+                    JFrame f = new JFrame();
+                    JOptionPane.showMessageDialog(f,"Error Login in, check your credentials again :(","Alert",JOptionPane.WARNING_MESSAGE);
+
+                }
+                }     
+        });
+
+   }
+    public void close(){
+        WindowEvent windClosingEvent = new WindowEvent(this, WindowEvent.WINDOW_CLOSING);
+        Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(windClosingEvent);
+        
+    }
+ 
+    public static void main(String args[]) {
+
+        new InsulinGlucagonPump();
+
+    }
+
+    public static Boolean login(String username, String password, Boolean status )  {
+
+        try{
+            String driver = "com.mysql.cj.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:3306/InsulinPump"; // Provide different db name if u want to call in a different db
+            String dbUsername = "mysql"; //if using localhost, provide your mysql username here
+            String dbPassword = "mysql"; // if running localhost, provide your mysql password here
+            Class.forName(driver);
+            Connection conn = DriverManager.getConnection(url, dbUsername, dbPassword);
+            System.out.println("Connected to the database successfully");
+
+            Statement stmt = conn.createStatement();
+            String sql = String.format( "SELECT password from users WHERE username = '%s'", username);
+            ResultSet rs = stmt.executeQuery(sql);
+            if (rs.next()){
+                String passwordHash = rs.getString("password");
+                System.out.println(passwordHash);
+
+                String hashPassword = MD5Hash.HashPassword(password);
+
+                // compare two hash passwords for login
+
+                // Login confirmed
+                status = Objects.equals(passwordHash, hashPassword);
+
+
+            }
+
+
+
+
+
+        }catch(Exception e){
+            System.out.println("Oops, error!");
+            JFrame g = new JFrame();
+            JOptionPane.showMessageDialog(g,"Oops, Error, check your connection again :(","Alert",JOptionPane.WARNING_MESSAGE);
+
+            e.printStackTrace();
+        }
+
+        return status;
+    }
+
+}
